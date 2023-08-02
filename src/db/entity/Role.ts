d18dfd6f227e328a1ab454eb9f33a2d073f02933
oleh19127 <oleh19127@gmail.com>
2023-08-02 22:15:01 +0300

    Create Role.ts

---
+++ src/db/entity/Role.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { User } from "./User";
+
+@Entity()
+export class Role {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: "USER" })
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Column()
+  userId: number;
+
+  @ManyToOne(() => User, (user) => user.roles)
+  user: User;
+}
