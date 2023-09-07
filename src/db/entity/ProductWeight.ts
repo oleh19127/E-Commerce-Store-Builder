d2ef7b2bbcbefa49b81b7dfc9890e19689a5350e
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-09-07 12:39:14 +0300

    Create ProductWeight.ts

---
+++ src/db/entity/ProductWeight.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "./Product";
+
+@Entity()
+export class ProductWeight {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  productId: number;
+
+  @Column()
+  kg: number;
+
+  @Column()
+  lb: number;
+
+  @OneToOne(() => ProductWeight, (productWeight) => productWeight.product)
+  product: Product;
+}
