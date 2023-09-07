4c27fcb495db987b80937a4ad32d1b23f795c72d
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-09-07 12:39:48 +0300

    Update data-source.ts

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -2,6 +2,14 @@ import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 import { Role } from "./entity/Role";
 import { Cart } from "./entity/Cart";
+import { CartProduct } from "./entity/CartProduct";
+import { ProductCategory } from "./entity/ProductCategory";
+import { ProductColor } from "./entity/ProductColor";
+import { ProductImage } from "./entity/ProductImage";
+import { ProductSize } from "./entity/ProductSize";
+import { ProductStyle } from "./entity/ProductStyle";
+import { ProductTag } from "./entity/ProductTag";
+import { ProductWeight } from "./entity/ProductWeight";
 
 const port = process.env.DATA_BASE_PORT
   ? parseInt(process.env.DATA_BASE_PORT)
@@ -10,15 +18,27 @@ const port = process.env.DATA_BASE_PORT
 export const AppDataSource = new DataSource({
   type: "postgres",
   port: port, // for local and external
-  // host: process.env.DATA_BASE_HOST, // for local only
-  // username: process.env.DATA_BASE_USER_NAME, // for local only
-  // password: process.env.DATA_BASE_PASSWORD, // for local only
-  // database: process.env.DATA_BASE_NAME, // for local only
+  host: process.env.DATA_BASE_HOST, // for local only
+  username: process.env.DATA_BASE_USER_NAME, // for local only
+  password: process.env.DATA_BASE_PASSWORD, // for local only
+  database: process.env.DATA_BASE_NAME, // for local only
   synchronize: true,
-  ssl: true, // external only
+  // ssl: true, // external only
   logging: true,
-  entities: [User, Role, Cart],
+  entities: [
+    User,
+    Role,
+    Cart,
+    CartProduct,
+    ProductCategory,
+    ProductColor,
+    ProductImage,
+    ProductSize,
+    ProductStyle,
+    ProductTag,
+    ProductWeight,
+  ],
   subscribers: [],
   migrations: [],
-  url: process.env.DATA_BASE_URL, // external only
+  // url: process.env.DATA_BASE_URL, // external only
 });
