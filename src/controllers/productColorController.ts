1715fef993bc91a6eb90eae9847aa9c992a15424
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-09-15 22:27:16 +0300

    Create productColorController.ts

---
+++ src/controllers/productColorController.ts
@@ -0,0 +1,52 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { IProductColor } from "../interfaces/IProductColor";
+import { productColorService } from "../services/productColorService";
+
+class ProductColorController {
+  async createColor(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const { productId } = request.params as IProductColor;
+      const { colorValue } = request.body as IProductColor;
+      const result = await productColorService.createColor(
+        productId,
+        colorValue,
+      );
+      return reply.send(result);
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+
+  async getAllProductColors(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const { productId } = request.params as IProductColor;
+      const result = await productColorService.getAllProductColors(productId);
+      return reply.send(result);
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+
+  async updateColor(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const { id } = request.params as IProductColor;
+      const { colorValue } = request.body as IProductColor;
+      const result = await productColorService.updateColor(id, colorValue);
+      return reply.send(result);
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+
+  async deleteColor(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const { id } = request.params as IProductColor;
+      const result = await productColorService.deleteColor(id);
+      return reply.send(result);
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+}
+
+export const productColorController = new ProductColorController();
