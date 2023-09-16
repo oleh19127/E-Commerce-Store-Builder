046c9855d37c620c897000a9667cdb7215c4ca5a
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-09-16 14:54:22 +0300

    Create productSizeController.ts

---
+++ src/controllers/productSizeController.ts
@@ -0,0 +1,52 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { IProductSize } from "../interfaces/IProductSize";
+import { productSizeService } from "../services/productSizeService";
+
+class ProductSizeController {
+  async createSize(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const { productId } = request.params as IProductSize;
+      const { sizeName } = request.body as IProductSize;
+      const result = await productSizeService.createSize(productId, sizeName);
+      return reply.send(result);
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+
+  async getAllProductSizes(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const { productId } = request.params as IProductSize;
+      const result = await productSizeService.getAllProductSizes(productId);
+      return reply.send(result);
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+
+  async updateProductSize(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const { productId } = request.params as IProductSize;
+      const { sizeName } = request.body as IProductSize;
+      const result = await productSizeService.updateProductSize(
+        productId,
+        sizeName,
+      );
+      return reply.send(result);
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+
+  async deleteProductSize(request: FastifyRequest, reply: FastifyReply) {
+    try {
+      const { productId } = request.params as IProductSize;
+      const result = await productSizeService.deleteProductSize(productId);
+      return reply.send(result);
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+}
+
+export const productSizeController = new ProductSizeController();
