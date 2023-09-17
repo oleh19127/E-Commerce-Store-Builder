ffa4bd897a35abc8077c4906ac64b6c4e0bae829
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-09-17 16:27:08 +0300

    Create productTagService.ts

---
+++ src/services/productTagService.ts
@@ -0,0 +1,38 @@
+import { AppDataSource } from "../db/data-source";
+import { ProductTag } from "../db/entity/ProductTag";
+
+class ProductTagService {
+  private productTagRepository = AppDataSource.getRepository(ProductTag);
+
+  async createProductTag(productId: number, tagName: string) {
+    const tag = new ProductTag();
+    tag.productId = productId;
+    tag.tagName = tagName;
+    await this.productTagRepository.save(tag);
+    return tag;
+  }
+
+  async getAllProductTags(productId: number) {
+    return await this.productTagRepository.findAndCountBy({ productId });
+  }
+  async updateProductTag(id: number, tagName: string) {
+    const tag = await this.productTagRepository.findOneBy({ id });
+    if (tag === null) {
+      return "Tag not found";
+    }
+    tag.tagName = tagName;
+    await this.productTagRepository.save(tag);
+    return tag;
+  }
+  async deleteProductTag(id: number) {
+    const destroyedTag = await this.productTagRepository.delete(id);
+    if (destroyedTag.affected === 1) {
+      return "Tag successfully deleted!!!";
+    }
+    if (destroyedTag.affected === 0) {
+      return "There is no such tag to delete it!!!";
+    }
+  }
+}
+
+export const productTagService = new ProductTagService();
