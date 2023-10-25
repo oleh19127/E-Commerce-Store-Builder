23dfb0756120eedfe93443aec42ea80bd31c434c
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-10-25 19:10:37 +0300

    fix role paths

--- src/routes/role/index.ts
+++ src/routes/role/index.ts
@@ -2,12 +2,12 @@ import { FastifyPluginAsync } from 'fastify';
 import { roleController } from '../../controllers/roleController';
 
 const role: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.post('/:userId', roleController.createRole);
   fastify.get('/:userId', roleController.getAllUserRoles);
-  fastify.delete('/', roleController.deleteRole);
-  fastify.get('/', roleController.getAllRoles);
-  fastify.get('/get/one/:id', roleController.getOneRole);
-  fastify.put('/:id', roleController.updateRole);
+  // fastify.post('/:userId', roleController.createRole);
+  // fastify.delete('/', roleController.deleteRole);
+  // fastify.get('/', roleController.getAllRoles);
+  // fastify.get('/get/one/:id', roleController.getOneRole);
+  // fastify.put('/:id', roleController.updateRole);
 };
 
 export default role;
