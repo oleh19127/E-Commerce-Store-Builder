deb22212ff07722c5124ce58bf0a449f0302d9f7
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-10-27 16:36:25 +0300

    added make-admin post method

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -2,8 +2,9 @@ import { FastifyPluginAsync } from 'fastify';
 import { userController } from '../../controllers/userController';
 
 const user: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/all/:userId', userController.getAllUsers);
   fastify.get('/one/:id', userController.getOne);
+  fastify.get('/all', userController.getAllUsers);
+  fastify.post('/make-admin/:id', userController.makeAdmin);
   fastify.get('/auth', userController.auth);
   fastify.post('/registration', userController.createUser);
   fastify.post('/login', userController.login);
