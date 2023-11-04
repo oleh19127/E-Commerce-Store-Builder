529438094f9d9a2208be1ac0a6d6b5cc19830418
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-11-04 17:54:09 +0200

    connection to external database

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -9,13 +9,13 @@ const port = process.env.DATA_BASE_PORT
 export const AppDataSource = new DataSource({
   type: 'postgres',
   port: port, // for local and external
-  host: process.env.DATA_BASE_HOST, // for local only
-  username: process.env.DATA_BASE_USER_NAME, // for local only
-  password: process.env.DATA_BASE_PASSWORD, // for local only
-  database: process.env.DATA_BASE_NAME, // for local only
+  // host: process.env.DATA_BASE_HOST, // for local only
+  // username: process.env.DATA_BASE_USER_NAME, // for local only
+  // password: process.env.DATA_BASE_PASSWORD, // for local only
+  // database: process.env.DATA_BASE_NAME, // for local only
+  ssl: true, // external only
+  url: process.env.DATA_BASE_URL, // external only
   synchronize: true,
-  // ssl: true, // external only
-  // url: process.env.DATA_BASE_URL, // external only
   logging: true,
   entities: [
     User,
