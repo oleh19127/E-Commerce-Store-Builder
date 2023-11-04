c6c06fb9c7dbb9f39ecab20971c5d48c489d0171
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-11-04 17:55:41 +0200

    add swagger shemas to some routes

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -2,14 +2,247 @@ import { FastifyPluginAsync } from 'fastify';
 import { userController } from '../../controllers/userController';
 
 const user: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/one/:id', userController.getOne);
-  fastify.get('/all', userController.getAllUsers);
-  fastify.post('/make-admin/:id', userController.makeAdmin);
+  fastify.get(
+    '/one/:id',
+    {
+      schema: {
+        description: 'Get one user by id',
+        tags: ['user'],
+        params: {
+          type: 'object',
+          properties: {
+            id: { type: 'number' },
+          },
+        },
+        response: {
+          200: {
+            type: 'object',
+            properties: {
+              id: { type: 'number' },
+              email: { type: 'string' },
+              password: { type: 'string' },
+              created_at: { type: 'string', format: 'date-time' },
+              updated_at: { type: 'string', format: 'date-time' },
+              roles: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'number' },
+                    roleName: { type: 'string' },
+                    created_at: { type: 'string', format: 'date-time' },
+                    updated_at: { type: 'string', format: 'date-time' },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    userController.getOne,
+  );
+  fastify.get(
+    '/all',
+    {
+      schema: {
+        description: 'Get all users',
+        tags: ['user'],
+        response: {
+          200: {
+            type: 'array',
+            items: {
+              type: 'array',
+              items: {
+                type: 'object',
+                properties: {
+                  id: { type: 'number' },
+                  email: { type: 'string' },
+                  password: { type: 'string' },
+                  created_at: { type: 'string', format: 'date-time' },
+                  updated_at: { type: 'string', format: 'date-time' },
+                  roles: {
+                    type: 'array',
+                    items: {
+                      type: 'object',
+                      properties: {
+                        id: { type: 'number' },
+                        roleName: { type: 'string' },
+                        created_at: { type: 'string', format: 'date-time' },
+                        updated_at: { type: 'string', format: 'date-time' },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    userController.getAllUsers,
+  );
+
+  fastify.post(
+    '/registration',
+    {
+      schema: {
+        description: 'Create user',
+        tags: ['user'],
+        security: [{ apiKey: [] }],
+        body: {
+          type: 'object',
+          properties: {
+            email: { type: 'string' },
+            password: { type: 'string' },
+          },
+        },
+        response: {
+          201: {
+            description: 'Successful response',
+            type: 'object',
+            properties: {
+              createdUser: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    userController.createUser,
+  );
+
+  fastify.put(
+    '/:id',
+    {
+      schema: {
+        description: 'Update user info',
+        tags: ['user'],
+        security: [{ apiKey: [] }],
+        params: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'number',
+              description: 'user id',
+            },
+          },
+        },
+        body: {
+          type: 'object',
+          properties: {
+            email: { type: 'string' },
+            password: { type: 'string' },
+          },
+        },
+        response: {
+          201: {
+            description: 'Successful response',
+            type: 'object',
+            properties: {
+              id: { type: 'number' },
+              email: { type: 'string' },
+              password: { type: 'string' },
+              created_at: { type: 'string', format: 'date-time' },
+              updated_at: { type: 'string', format: 'date-time' },
+            },
+          },
+        },
+      },
+    },
+    userController.update,
+  );
+  fastify.delete(
+    '/:id',
+    {
+      schema: {
+        description: 'Delete user by id',
+        tags: ['user'],
+        params: {
+          type: 'object',
+          properties: {
+            id: { type: 'number' },
+          },
+        },
+        response: {
+          200: {
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    userController.delete,
+  );
+
+  fastify.post(
+    '/make-admin/:id',
+    {
+      schema: {
+        description: 'Make user admin by id',
+        tags: ['user'],
+        params: {
+          type: 'object',
+          properties: {
+            id: { type: 'number' },
+          },
+        },
+        response: {
+          200: {
+            type: 'object',
+            properties: {
+              id: { type: 'number' },
+              email: { type: 'string' },
+              password: { type: 'string' },
+              created_at: { type: 'string', format: 'date-time' },
+              updated_at: { type: 'string', format: 'date-time' },
+              roles: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'number' },
+                    roleName: { type: 'string' },
+                    created_at: { type: 'string', format: 'date-time' },
+                    updated_at: { type: 'string', format: 'date-time' },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    userController.makeAdmin,
+  );
+  fastify.post(
+    '/login',
+    {
+      schema: {
+        description: 'Make user admin by id',
+        tags: ['user'],
+        body: {
+          type: 'object',
+          properties: {
+            email: { type: 'string' },
+            password: { type: 'string' },
+          },
+        },
+        response: {
+          201: {
+            description: 'Successful response',
+            type: 'object',
+            properties: {
+              result: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    userController.login,
+  );
   fastify.get('/auth', userController.auth);
-  fastify.post('/registration', userController.createUser);
-  fastify.post('/login', userController.login);
-  fastify.put('/:id', userController.update);
-  fastify.delete('/:id', userController.delete);
 };
 
 export default user;
