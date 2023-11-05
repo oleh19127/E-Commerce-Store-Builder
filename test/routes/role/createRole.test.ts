333862feb5082727375838599e683f5d32406f8a
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-11-05 14:26:39 +0200

    create  role test

---
+++ test/routes/role/createRole.test.ts
@@ -0,0 +1,24 @@
+import { test } from 'tap';
+import { build } from '../../helper';
+import { statusCodes } from '../../../src/static-helpers/status-codes';
+import { roleService } from '../../../src/services/roleService';
+
+test('Create role route', async (t) => {
+  const app = await build(t);
+  const res = await app.inject({
+    url: '/role',
+    method: 'POST',
+    payload: { roleName: 'DESIGNER' },
+  });
+  t.equal(
+    res.statusCode,
+    statusCodes.CREATED_201,
+    'Create role operation should return status 201',
+  );
+  const resPayload = JSON.parse(res.payload);
+  t.same(resPayload.roleName, 'DESIGNER');
+
+  t.after(async () => {
+    await roleService.deleteRole(resPayload.id);
+  });
+});
