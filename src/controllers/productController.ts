738728abe88366f2d8d25877d020f581baa435a0
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-11-17 19:57:38 +0200

    refactor: add status codes and delete some  params

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,67 +1,64 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { IProduct } from '../interfaces/IProduct';
 import { productService } from '../services/productService';
+import { statusCodes } from '../static-helpers/status-codes';
 
 class ProductController {
   async createProduct(request: FastifyRequest, reply: FastifyReply) {
     try {
       const { sku, price, subtitle, title } = request.body as IProduct;
-      const { userId } = request.params as IProduct;
       const result = await productService.createProduct(
         sku,
         price,
         subtitle,
         title,
-        userId,
       );
-      return reply.send(result);
+      return reply.status(statusCodes.CREATED_201).send(result);
     } catch (e) {
       return reply.send(e);
     }
   }
 
-  async getAll(request: FastifyRequest, reply: FastifyReply) {
+  async getAllProducts(_request: FastifyRequest, reply: FastifyReply) {
     try {
       const result = await productService.getAll();
-      return reply.send(result);
+      return reply.status(statusCodes.OK_200).send(result);
     } catch (e) {
       return reply.send(e);
     }
   }
 
-  async getOne(request: FastifyRequest, reply: FastifyReply) {
+  async getOneProduct(request: FastifyRequest, reply: FastifyReply) {
     try {
-      const { productId } = request.params as IProduct;
-      const result = await productService.getOne(productId);
-      return reply.send(result);
+      const { id } = request.params as IProduct;
+      const result = await productService.getOne(id);
+      return reply.status(statusCodes.OK_200).send(result);
     } catch (e) {
       return reply.send(e);
     }
   }
-  async update(request: FastifyRequest, reply: FastifyReply) {
+  async updateProduct(request: FastifyRequest, reply: FastifyReply) {
     try {
-      const { productId } = request.params as IProduct;
-      const { sku, price, subtitle, title, cartProductId } =
-        request.body as IProduct;
+      const { id } = request.params as IProduct;
+      const { sku, price, subtitle, title } = request.body as IProduct;
       const result = await productService.update(
-        productId,
+        id,
         sku,
         price,
         subtitle,
         title,
-        cartProductId,
       );
-      return reply.send(result);
+      return reply.status(statusCodes.OK_200).send(result);
     } catch (e) {
       return reply.send(e);
     }
   }
 
-  async delete(request: FastifyRequest, reply: FastifyReply) {
+  async deleteProduct(request: FastifyRequest, reply: FastifyReply) {
     try {
-      const { productId } = request.params as IProduct;
-      const result = await productService.delete(productId);
-      return reply.send(result);
+      const { id } = request.params as IProduct;
+      const result = await productService.delete(id);
+      return reply.status(statusCodes.OK_200).send(result);
     } catch (e) {
       return reply.send(e);
     }
