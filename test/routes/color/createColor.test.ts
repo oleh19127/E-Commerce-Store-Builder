1b0e1f0be4dd37e5523ff69a607bdf0642d6d129
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-11-17 20:18:58 +0200

    test: create color test

---
+++ test/routes/color/createColor.test.ts
@@ -0,0 +1,24 @@
+import { test } from 'tap';
+import { build } from '../../helper';
+import { statusCodes } from '../../../src/static-helpers/status-codes';
+import { colorService } from '../../../src/services/colorService';
+
+test('Create color route', async (t) => {
+  const app = await build(t);
+  const res = await app.inject({
+    url: '/color',
+    method: 'POST',
+    payload: { colorValue: 'green' },
+  });
+  t.equal(
+    res.statusCode,
+    statusCodes.CREATED_201,
+    'Create role operation should return status 201',
+  );
+  const resPayload = JSON.parse(res.payload);
+  t.same(resPayload.colorValue, 'green');
+
+  t.after(async () => {
+    await colorService.deleteColor(resPayload.id);
+  });
+});
