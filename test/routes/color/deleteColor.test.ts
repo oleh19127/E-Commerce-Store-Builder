dd930ce6a728ac6699dcf623b9dc60f9299cd4f8
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2023-11-17 20:19:22 +0200

    test: delete color test

---
+++ test/routes/color/deleteColor.test.ts
@@ -0,0 +1,18 @@
+import { test } from 'tap';
+import { build } from '../../helper';
+import { statusCodes } from '../../../src/static-helpers/status-codes';
+import { colorService } from '../../../src/services/colorService';
+
+test('Delete color route', async (t) => {
+  const app = await build(t);
+  const color = await colorService.createColor('delete color');
+  const res = await app.inject({
+    url: `/color/${color.id}`,
+    method: 'DELETE',
+  });
+  t.equal(
+    res.statusCode,
+    statusCodes.OK_200,
+    'Delete color operation should return status 200',
+  );
+});
